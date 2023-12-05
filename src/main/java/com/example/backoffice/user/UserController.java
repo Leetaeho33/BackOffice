07580e63b7b12eca56b8a571d859ab5793447078
taeholee <lth1993@naver.com>
2023-12-05 17:25:29 +0900

    fix : 로그인 예외시 인증토큰 미발급

--- src/main/java/com/example/backoffice/user/UserController.java
+++ src/main/java/com/example/backoffice/user/UserController.java
@@ -9,6 +9,7 @@ import com.example.backoffice.user.entity.UserDetailsImpl;
 import com.example.backoffice.user.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +30,11 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<CommonResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO,
                                                    HttpServletResponse response){
+        try {
+            userService.login(loginRequestDTO);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(new CommonResponseDTO(e.getMessage(), HttpStatus.BAD_REQUEST.value()));
+        }
         response.setHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(loginRequestDTO.getUsername()));
         return userService.login(loginRequestDTO);
     }
