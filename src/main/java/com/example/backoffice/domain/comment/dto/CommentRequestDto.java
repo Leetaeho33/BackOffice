8e665a0ab4b7cf4f1d234acb8220aa5e6d1e01b1
guncow0603 <guncow0603@gmail.com>
2023-12-07 09:39:48 +0900

    카멜케이스로 수정,setter부분 빌더로 수정,인증인가 추가,user와 관계설정

--- src/main/java/com/example/backoffice/domain/comment/dto/CommentRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.backoffice.domain.comment.dto;
-
-import lombok.Getter;
-
-@Getter
-public class CommentRequestDto {
-    private Long comment_like_count;  // 댓글 좋아요 수
-    private String texts;              // 댓글 내용
-}
