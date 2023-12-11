e24d6c0e4fbfa1f5fc318122db36af2e5b726458
BMDK9 <qnrakdlzm77@naver.com>
2023-12-11 10:54:27 +0900

    #35 refactor : 게시물 좋아요 수정 완료

--- src/main/java/com/example/backoffice/domain/postLike/service/PostLikeService.java
+++ src/main/java/com/example/backoffice/domain/postLike/service/PostLikeService.java
@@ -14,6 +14,7 @@ import static com.example.backoffice.domain.postLike.constant.PostLikeConstant.D
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PostLikeService {
 
     private final PostService postService;
